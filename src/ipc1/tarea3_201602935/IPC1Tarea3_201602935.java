4cc5ab89e893cfb49d23199c5fd881c85db58170
lourd <lourd@192.168.0.5>
2018-08-13 23:27:32 -0600

    Estoy trabajando en mi tarea

--- src/ipc1/tarea3_201602935/IPC1Tarea3_201602935.java
+++ src/ipc1/tarea3_201602935/IPC1Tarea3_201602935.java
@@ -1,21 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ipc1.tarea3_201602935;
 
-/**
- *
- * @author lourd
- */
+import java.util.Scanner;
+
 public class IPC1Tarea3_201602935 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner var = new Scanner(System.in);
+        System.out.println("1. Usuarios");
+        System.out.println("2. Contador de dígitos repetidos");
+        System.out.println("3. Tres números de mayor a menor");
+        System.out.println("4. Calcular promedio");
+        System.out.println("5. Salir");
+        String eleccion = var.nextLine();
+        //menu principal
+        
+        switch(eleccion){
+            case "1": {
+                Submenu a = new Submenu();
+                a.RegistroDeUsuario();
+                break;
+            }
+            case "2":{
+                Submenu b = new Submenu();
+                b.Contador();
+                break;
+            }
+            case "3":{
+                Submenu c = new Submenu();
+                c.TresNumeros();
+                break;
+            }
+            case "4":{
+                Submenu d = new Submenu();
+                d.Promedio();
+                break;
+            }
+            default:
+                System.out.println("No es una opción válida");
+                break;
+                
+    }
+        
     }
     
 }
