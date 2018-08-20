bdf96d1af08064f887fe9a882cb30e65e392503a
lourd <lourd@192.168.0.5>
2018-08-19 22:11:53 -0600

    Estoy trabajando en mi tarea

--- src/ipc1/tarea3_201602935/IPC1Tarea3_201602935.java
+++ src/ipc1/tarea3_201602935/IPC1Tarea3_201602935.java
@@ -7,15 +7,16 @@ public class IPC1Tarea3_201602935 {
        a.menu();
     }
     public void menu(){
-      System.out.println("1. Usuarios");
+        System.out.println("1. Usuarios");
         System.out.println("2. Contador de dígitos repetidos");
         System.out.println("3. Tres números de mayor a menor");
         System.out.println("4. Calcular promedio");
         System.out.println("5. Salir");
         Scanner var = new Scanner(System.in);                
         String eleccion = var.nextLine();
-        //IPC1Tarea3_201602935 a=new IPC1Tarea3_201602935();
-        switch(eleccion){
+        boolean salir = false;
+        while(!salir){
+            switch(eleccion){
             case "1": 
                 a.RegistroDeUsuario();
                 break;
@@ -28,9 +29,13 @@ public class IPC1Tarea3_201602935 {
             case "4":
                 a.Promedio();
                 break;
+            case "5":
+                salir = true;
+                break;
             default:
                 System.out.println("Opción inválida");
                 break;
+           }
         }
     }
     public String [] user=new String[5]; 
@@ -102,48 +107,52 @@ public class IPC1Tarea3_201602935 {
        String eleccion = var.nextLine();
        switch(eleccion){
            case "1":
-               a.Case1();
+               a.GuardarNumero();
                a.Contador();
                break;
            case "2":
                num = Integer.toString(numero);//Convertir un entero a un string
                char[] valores =num.toCharArray();// de un string a un char
                int i;
-               System.out.println("Ingrese la posición que desea");
+               System.out.println("Ingrese la posición que desea de 0-9");
                i = var.nextInt();
                if(i< valores.length){
-                    System.out.println("El Valor " + valores[i]+" Ocupa el : " +(i)+" Lugar");
+                    System.out.println("EL VALOR " + valores[i]+" OCUPA EL " +(i)+" Lugar");
                    }
                    else{
-                       System.out.println("Esa posición no existe");
+                       System.out.println("ESA POSICIÓN NO EXISTE");
                    }
                a.Contador();
                break;
            case "3":
-               a.Case3();
+               a.NumeroDeDigitos();
                a.Contador();
                break;
            case "4":
-              a.menu();
+               a.menu();
+               a.Contador();
+               break;
            default:
-               System.out.println("Opción inválida");
+               System.out.println("Opción Inválida");
+               a.Contador();
                break;
        }
    }
-    public void Case1(){
+    public void GuardarNumero(){
        System.out.println("Ingresar un número");
-               numero = var.nextInt();
-               a.Contador();
+       numero = var.nextInt();
+       System.out.println("Número registrado");       
    }
-    public void Case3(){
+    public void NumeroDeDigitos(){
         while(numero !=0){
                    numero = numero/10;
                    contador++;
                }
                System.out.println(contador + " digitos ");
     }
-    private int num1 = 0, num2= 0, num3= 0, menor= 0, medio= 0, mayor= 0;
+    public int num1 = 0, num2= 0, num3= 0, menor= 0, medio= 0, mayor= 0;
     public void TresNumeros(){
+        
        // Tres numeros de mayor a menor
        Scanner var = new Scanner(System.in);
        System.out.println("1. Ingresar numeros");
@@ -158,18 +167,18 @@ public class IPC1Tarea3_201602935 {
                num2 = var.nextInt();
                System.out.println("Ingrese tercer número ");
                num3 = var.nextInt();
-               System.out.println("Números ingresados:" + num1+" "+num2+" "+num3);//revisar
+               System.out.println(  num1+" "+num2+" "+num3);//revisar
                if(num1<num2 && num2<num3){
                    menor = num1;
                    medio = num2;
                    mayor = num3;
                }
-               else if(num1<num3&&num3<2){
+               else if(num1<num3&&num3<num2){
                    menor = num1;
                    medio = num3;
                    mayor = num2;
                }
-               else if(num2<num1&& num1<3){
+               else if(num2<num1&& num1<num3){
                    menor = num2;
                    medio = num1;
                    mayor = num3;
@@ -186,19 +195,22 @@ public class IPC1Tarea3_201602935 {
                    medio = num2;
                    mayor = num1; 
                }
-               System.out.println( "Ordeanados de mayor a menor" +mayor  +","+  medio + ","+  menor );
+               System.out.println( "Ordeanados de mayor a menor " +mayor  +","+  medio + ","+  menor );
                a.TresNumeros();
                break;
            case 2:
-               System.out.println( "Ordeanados de menor a mayor" +menor  +","+  medio + ","+  mayor );
+               System.out.println( "Ordeanados de menor a mayor " +menor  +","+  medio + ","+  mayor );
                a.TresNumeros();
                break;
            case 3:
                a.menu();
                break;
+           default:
+               System.out.println("Opcion invalida");
+               break;
         }
     }
-    String[] encabezado = {"ID", "N1","N2","N3", "N4", "PROM"}; 
+    String[] encabezado = {"   ID  ", "   N1  ","   N2  ","   N3  ", "   N4  ", "  PROM  "}; 
     int [][] matriz = new int[6][6] ;
     public void Promedio(){
       System.out.println("1. Ingresar notas");
@@ -208,7 +220,7 @@ public class IPC1Tarea3_201602935 {
       switch(eleccion){
           case 1:
               for( int i=0;i<6;i++){
-                     System.out.println("Ingrese ID" + i );
+                     System.out.println("Ingrese ID " + (i+1));
                      matriz[i][0] = var.nextInt();
                      System.out.println("Ingresar  primer nota");
                      matriz[i][1] = var.nextInt();
@@ -220,28 +232,28 @@ public class IPC1Tarea3_201602935 {
                      matriz[i][4]= var.nextInt();
                      matriz[i][5]=((matriz[i][1]+matriz[i][2]+matriz[i][3]+matriz[i][4])/4);
                 }
+              System.out.println("NOTAS GUARDADAS");
               a.Promedio();
                 break;
           case 2:
-                a.imprimir(matriz, encabezado);
+                a.imprimir( matriz, encabezado);
                 break;
           case 3:
               a.menu();
               break;
           default:
               System.out.println("Opción inválida");
-              a.Promedio();
               break;
         }
     }
     public void imprimir(int matriz[][], String encabezado[]){
         for(int i=0; i<6;i++){
-            System.out.print("|" + encabezado[i] + "|");
+            System.out.print(" |" + encabezado[i] + "| ");
         }
         System.out.println(" ");
         for (int i = 0; i<6; i++){
             for(int j=0; j<6; j++){
-                System.out.print(" | " + matriz[i][j] + " | ");
+                System.out.print(" |   " + matriz[i][j] + "   | ");
             }
             System.out.println("");
         }
